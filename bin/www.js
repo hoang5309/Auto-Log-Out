0903cf178e84e7ad2d607d096182690d1c4948b1
steveAyre34 <stephen.ayre@aol.com>
2018-12-04 16:50:59 -0500

    Load Node JS

--- bin/www.js
+++ bin/www.js
@@ -4,8 +4,10 @@ var path    = require("path");
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/../index.html'));
-    //__dirname : It will resolve to your project folder.
 }); 
+app.get('/login', function(req, res){
+    res.send('this is login page');
+});
 app.listen(8080);
   
 console.log("Running at Port 8080");
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){    
     $('#loginBtn').click(function(){
-        
+        console.log('Hello');
+        $.ajax({
+            method: "POST",
+            type: "json",
+            url: "localhost:8080/login",
+            data:{
+                username: $('.user').val(),
+                password: $('.pass').val()
+            },
+            success:function(data){
+                console.log("Hello");
+            }
+        });
     });
 });
