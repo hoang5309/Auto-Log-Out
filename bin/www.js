723a5fdf9d4804fa2b2fa16b139b35514b83dc08
steveAyre34 <stephen.ayre@aol.com>
2018-12-04 16:21:12 -0500

    Node Server

---
+++ bin/www.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/../index.html'));
+    //__dirname : It will resolve to your project folder.
+}); 
+app.listen(8080);
+  
+console.log("Running at Port 8080");
